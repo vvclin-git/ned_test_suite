0cfa36af3b3e6a3a80930130fd3d90fcfb05aedd
vvclin <vvclin@gmail.com>
2022-02-08 18:13:53 +0800

    msgbox test

--- Frames/NetsFrame.py
+++ Frames/NetsFrame.py
@@ -15,40 +15,46 @@ class NetsFrame(Frame):
         
         output = Frame(self, style='Output.TFrame', width=980, height=960, padding=(10, 10))   
         output.grid(row=0, column=0, rowspan=2, sticky="NEWS")
-
-        # preview_frame = LabelFrame(output, text='Preview Image', width=960, height=740, relief='groove', borderwidth=2)
+        
         preview_frame = LabelFrame(output, text='Preview Image', width=960, height=740, style='TLabelframe')        
         preview_frame.pack(side='top')
         
         img = np.zeros([720, 960, 3], dtype=np.uint8)
         cv2.putText(img, 'Preview Image', (400, 330), fontFace=cv2.FONT_HERSHEY_SIMPLEX, color=(0, 255, 0), fontScale=1, thickness=1)        
         img = Image.fromarray(img)
-        preview_img = ImageTk.PhotoImage(img)        
+        self.preview_img = ImageTk.PhotoImage(img)        
         
-        preview_img_box = tk.Canvas(preview_frame, width=960, height=720, relief='flat', highlightthickness=0, borderwidth=0)
-        preview_img_box.create_image(0, 0, image=preview_img, anchor=tk.NW)                           
-        preview_img_box.image = preview_img
-        preview_img_box.pack()
+        self.preview_canvas = tk.Canvas(preview_frame, width=960, height=720, relief='flat', highlightthickness=0, borderwidth=0)
+        self.preview_img_box = self.preview_canvas.create_image(0, 0, image=self.preview_img, anchor=tk.NW)                           
+        self.preview_canvas.image = self.preview_img
+        self.preview_canvas.pack()
         
         msg_frame = LabelFrame(output, width=960, height=180, text='Output Message', style='TLabelframe')        
         msg_frame.pack(side='bottom')
         msg_frame.pack_propagate(0)
 
-        msg_output = Label(msg_frame, textvariable=self.msg, style='MessageBox.TLabel')
-        msg_output.pack(side=tk.LEFT, expand=True, fill='both')
+        self.msg_output = Label(msg_frame, textvariable=self.msg, style='MessageBox.TLabel')
+        self.msg_output.pack(side=tk.LEFT, expand=True, fill='both')
         msg_scroll = Scrollbar(msg_frame, orient=tk.VERTICAL)
         msg_scroll.pack(side=tk.RIGHT, fill=tk.Y)
         
-        settings = Frame(self, style='Settings.TFrame', width=500, height=720)
-        settings.grid(row=0, column=1)
-        settings.columnconfigure(0, weight=1)
-        settings.rowconfigure(0, weight=1)
-        
-        buttons = Frame(self, style='Buttons.TFrame', width=500, height=240)
-        buttons.grid(row=1, column=1)
-        buttons.columnconfigure(0, weight=1)
-        buttons.rowconfigure(0, weight=1)
+        self.settings = Frame(self, style='Settings.TFrame', width=500, height=720)
+        self.settings.grid(row=0, column=1)
 
+        
+        self.buttons = Frame(self, style='Buttons.TFrame', width=500, height=240)
+        self.buttons.grid(row=1, column=1)
+
+    def console(self, msg):
+        old_msg = self.msg.get()
+        new_msg = old_msg + '\n' + msg
+        self.msg.set(new_msg)
+        return
+    
+    def update_img(self, img):
+        self.preview_canvas.itemconfig(self.preview_img_box, image=img)
+        self.preview_canvas.image = img
+        return
 
 
 
--- Frames/TestCharts.py
+++ Frames/TestCharts.py
@@ -1,18 +1,34 @@
 import imp
-import tkinter as tk
-from Frames.NetsFrame import NetsFrame
-from tkinter.ttk import *
+from Frames.NetsFrame import *
+# from tkinter.ttk import *
 
 class TestCharts(NetsFrame):
     def __init__(self, window):
         super().__init__(window)
+        print(self.msg.get())
         
-        
+        img1 = ImageTk.PhotoImage(file='.\Temp\img01.png')
+        img2 = ImageTk.PhotoImage(file='.\Temp\img02.png')
+        img3 = ImageTk.PhotoImage(file='.\Temp\img03.png')
+        self.img_list = [img1, img2, img3]
 
+        img_test_btn = Button(self.buttons, text='Change Image', command=self.rotate_imgs)
+        img_test_btn.pack()
+
+        msg_test_btn = Button(self.buttons, text='Update Message', command=self.update_msg_test)
+        msg_test_btn.pack()
 
         # # Styling
         # self["style"] = "Background.TFrame"        
-
+    
+    def rotate_imgs(self):
+        self.update_img(self.img_list[0])
+        self.img_list.append(self.img_list.pop(0))
+        return
+        
+    def update_msg_test(self):
+        self.console('Message Text Update Test')
+        return
         
     
     
